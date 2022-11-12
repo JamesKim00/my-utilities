4ebba7fd2eabf0541478950ba3a3b857a1290913
James Kim <jameskimjaewon.abc@gmail.com>
2022-11-12 11:06:23 +0900

    Added tailwind with sample

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<div class="grid h-screen place-items-center">
+	<div class="rounded overflow-hidden shadow-lg p-10 items-center mx-10">
+		<slot />
+		<div class="px-6 py-4">
+			<div class="font-bold text-xl mb-2">More content</div>
+			<p class="text-gray-700 text-base">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores
+				et perferendis eaque, exercitationem praesentium nihil.
+			</p>
+		</div>
+		<div class="px-6 pt-4 pb-2">
+			<span
+				class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+				>#photography</span
+			>
+			<span
+				class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+				>#travel</span
+			>
+			<span
+				class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+				>#winter</span
+			>
+		</div>
+	</div>
+</div>
