8b78a7298561b820b715611c882986161edecb2b
James Kim <jameskimjaewon.abc@gmail.com>
2022-11-17 12:01:53 +0900

    VERCEL

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-static';
+// import adapter from '@sveltejs/adapter-static';
+import vercel from '@sveltejs/adapter-vercel';
 import preprocess from 'svelte-preprocess';
 
 // const prod = process.env.NODE_ENV === 'prod';
@@ -12,12 +13,16 @@ const config = {
 	// for more information about preprocessors
 	preprocess: [preprocess({ postcss: true })],
 	kit: {
-		adapter: adapter({
-			// pages: 'docs',
-			// assets: 'docs',
-			// fallback: null,
-			// precompress: false
-		})
+		// adapter: adapter({
+		// 	pages: 'public',
+		// 	assets: 'public'
+		// 	// pages: 'docs',
+		// 	// assets: 'docs',
+		// 	// fallback: null,
+		// 	// precompress: false
+		// })
+
+		adapter: vercel({})
 		// paths: { base: '/my-utilities' }
 		// trailingSlash: 'always',
 	}
